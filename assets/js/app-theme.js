f25de351eeefe49280197aae2ef47838f9ab88e7
pico.dev <pico.dev@gmail.com>
2017-06-30 19:18:43 +0200

    Site update at 2017-06-30T19:18:43+02:00

---
+++ assets/js/app-theme.js
@@ -0,0 +1,34 @@
+define(['jquery'], function($) {
+    // Background
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    var spec = {
+        backgrounds: [
+            'confectionary.png',
+            'dimension.png',
+            'skulls.png',
+            'tree_bark.png',
+            'contemporary_china_2.png',
+            'eight_horns.png',
+            'swirl_pattern.png',
+            'upfeathers.png',
+            'contemporary_china.png',
+            'paisley.png',
+            'symphony.png'
+        ]
+    };
+
+    var i = random(0, spec.backgrounds.length - 1);
+    var b = spec.backgrounds[i];
+    $('body').css('background-image', 'url("assets/images/backgrounds/' + b + '")');
+
+    $("form[name='query']").on('submit', function() {
+      var query = $("input[name='q']", "form[name='query']").val();
+      var site = $("form[name='search']").data('site');
+      $("input[name='q']", "form[name='search']").val(query + ' site:' + site);
+      $("form[name='search']").submit();
+      return false;
+    });
+});
