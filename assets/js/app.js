d603fb2973121c70f359c6079dbec42d5387bc3a
pico.dev <pico.dev@gmail.com>
2019-05-11 23:08:13 +0200

    Site update at 2019-05-11T23:08:13+0200

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,7 +66,7 @@ require(['jquery'], function($) {
         $('div.adblock-sidebar-right').css('transform', 'translateX(' + translate + 'px)');
 
         var ads = $('ins.adsbygoogle', 'div.adblock-sidebar');
-        ads.eq(0).parent('div').css('height', '600px');
+        ads.eq(0).parent('div').css('height', '600px').css('margin-bottom', '10px');
         ads.eq(1).parent('div').css('height', '300px');
     }
 
