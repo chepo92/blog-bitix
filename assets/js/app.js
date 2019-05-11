beed9556f9c971f333f93cd164d2c5625d0782b1
pico.dev <pico.dev@gmail.com>
2019-05-11 21:52:46 +0200

    Site update at 2019-05-11T21:52:46+0200

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-require(['jquery', 'bowser'], function($, bowser) {
+require(['jquery'], function($) {
     function initAnalytics() {
         ga('send', 'event', 'client', 'protocol', window.location.protocol.replace(new RegExp(':|/', 'gi'), ''), {'nonInteraction': 1});
       
@@ -36,22 +36,38 @@ require(['jquery', 'bowser'], function($, bowser) {
     }
     
     function initAdsense() {
-        var browser = bowser.parse(window.navigator.userAgent);
-        var screenSizeNormal = window.screen.width >= 1920;
-        var desktop = browser.platform.type === 'desktop';
-        var sidebarAds = screenSizeNormal;
+        initAdSenseSidebar();
+        checkAdblock();
+    }
+
+    function initAdSenseSidebar() {
+        var sidebarAds = window.screen.width >= 1366;
 
         if (!sidebarAds) {
             $(".adblock-sidebar").remove();
+            return;
         }
 
+        var sidebarAdsSize = (window.screen.width >= 1920) ? 336 : (window.screen.width >= 1440) ? 160 : 120;
+        if (window.location.hash != null) {
+            var hash = parseInt(window.location.hash.substring(1));
+            sidebarAdsSize = (Number.isNaN(hash)) ? 120 : hash;
+        }
+        var translate = sidebarAdsSize + 30;
+
+        $("div.adblock-sidebar").css('width', sidebarAdsSize);
+        $("div.adblock-sidebar-left").css('transform', 'translateX(' + (-translate) + 'px)');
+        $("div.adblock-sidebar-right").css('transform', 'translateX(' + translate + 'px)');
+    }
+
+    function checkAdblock() {
         setTimeout(function() {
             var ads = $('ins.adsbygoogle');
             var adblock = (ads.length > 0 && ads.html().replace(/\s/g, '').length == 0);
             ga('send', 'event', 'client', 'adblock', (adblock) ? 'true' : 'false', {'nonInteraction': 1});
 
             if (adblock) {
-                var ad = (sidebarAds) ? ads.filter('[data-type="large-skycraper"]').last() : ads.filter('[data-type="billboard"]').first();
+                var ad = ads.filter('[data-type="billboard"], [data-type="leaderboard"]').first();
                 var html = [
                     '<div style="' + $(ad).attr('style') + '; height: 100%; text-align: left;" class="adblock">',
                     ' <p class="text-center"><strong><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Parece que tienes activado un bloqueador de anuncios</strong></p>',
