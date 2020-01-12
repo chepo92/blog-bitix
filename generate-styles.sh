1fde9f594633e9677cc8a72ecb68f526314996dd
pico.dev <pico.dev@gmail.com>
2020-01-12 16:45:10 +0100

    Pequeños retoques

--- generate-styles.sh
+++ generate-styles.sh
@@ -3,8 +3,9 @@ set -e
 
 npm run less-main
 npm run less-main-cookieconsent
-npm run minify
 hugo gen chromastyles --style=github > themes/bitix/static/assets/css/syntax.css
 
 cp themes/bitix/static/assets/css/syntax.css themes/bitix/layouts/partials/style-syntax.amp.css
 cp themes/bitix/static/assets/css/main.css themes/bitix/layouts/partials/style-main.amp.css
+
+npm run minify-amp
