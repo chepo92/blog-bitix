08ce043051bee38a6ab23344128a20dcda80464d
pico.dev <pico.dev@gmail.com>
2020-04-24 17:01:10 +0200

    Site update at 2020-04-24T17:01:10+0200

--- 2020/04/argumentos-de-linea-de-comandos-con-jcommander-en-una-aplicacion-java/code/Arguments.java
+++ 2020/04/argumentos-de-linea-de-comandos-con-jcommander-en-una-aplicacion-java/code/Arguments.java
@@ -7,39 +7,39 @@ import java.util.List;
 
 public class Arguments {
 
-  @Parameter
-  private List<String> parameters = new ArrayList<>();
+    @Parameter
+    private List<String> parameters = new ArrayList<>();
 
-  @Parameter(names = { "--required", "-r" }, required = true, description = "Required parameter")
-  private boolean required;
+    @Parameter(names = { "--required", "-r" }, required = true, description = "Required parameter")
+    private boolean required;
 
-  @Parameter(names = { "--optional", "-o" }, description = "Optional parameter")
-  private boolean optional = false;
+    @Parameter(names = { "--optional", "-o" }, description = "Optional parameter")
+    private boolean optional = false;
 
-  @Parameter(names = { "--values", "-v" }, variableArity = true, description = "Values parameter")
-  private List<String> values = new ArrayList<>();
+    @Parameter(names = { "--values", "-v" }, variableArity = true, description = "Values parameter")
+    private List<String> values = new ArrayList<>();
 
-  @Parameter(names = { "--help", "-h" }, description = "Help parameter")
-  private boolean help = false;
+    @Parameter(names = { "--help", "-h" }, description = "Help parameter")
+    private boolean help = false;
 
-  public List<String> getParameters() {
-    return parameters;
-  }
+    public List<String> getParameters() {
+        return parameters;
+    }
 
-  public boolean isRequired() {
-    return required;
-  }
+    public boolean isRequired() {
+        return required;
+    }
 
-  public boolean isOptional() {
-    return optional;
-  }
+    public boolean isOptional() {
+        return optional;
+      
 
-  public List<String> getValues() {
-    return values;
-  }
+    public List<String> getValues() {
+        return values;
+    }
 
-  public boolean isHelp() {
-    return help;
-  }
+    public boolean isHelp() {
+        return help;
+    }
 }
 
